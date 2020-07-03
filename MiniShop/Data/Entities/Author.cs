cb6c6a2831797003b23bd9e961a3c60cb2a8953e
Ngọc Quân Trần <tranngocquan95vn@gmail.com>
2020-07-03 22:45:17 +0700

    Initial commit

---
+++ MiniShop/Data/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MiniShop.Data.Entities
+{
+    [Table("Authors")]
+    public class Author
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AuthorId { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Name { get; set; }
+    }
+}
---
+++ MiniShop/Data/Repository/AuthorRepository.cs
@@ -0,0 +1,12 @@
+﻿using MiniShop.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniShop.Data.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+    }
+}
