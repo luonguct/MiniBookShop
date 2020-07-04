1080e5a38013f4610a46a84e934af8d68f800499
QuanTN <tranngocquan95vn@gmail.com>
2020-07-04 09:29:08 +0700

    Add ConnectionString and DbContext

---
+++ MiniShop/Data/DbContext/MiniShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiniShop.Data.Entities;
+
+namespace MiniShop.Data.DbContext
+{
+    public class MiniShopDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public MiniShopDbContext(DbContextOptions<MiniShopDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+    }
+}
