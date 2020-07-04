4f740f71cf5e3d94523748e6076cd907013aa2b6
QuanTN <tranngocquan95vn@gmail.com>
2020-07-04 08:54:26 +0700

    Adding api controller

---
+++ MiniShop/Controllers/AuthorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public string GetAuthors()
+        {
+            return "return list of authors";
+        }
+
+        [HttpGet("{id}")]
+        public string GetAuthor(int id)
+        {
+            return $"return single author with id of {id}";
+        }
+    }
+}
---
+++ MiniShop/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        [HttpGet]
+        public string GetBooks()
+        {
+            return "return list of books";
+        }
+
+        [HttpGet("{id}")]
+        public string GetBook(int id)
+        {
+            return $"return single book with id of {id}";
+        }
+    }
+}
