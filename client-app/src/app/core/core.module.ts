1d9731b2c331b1e915007f6477be5214393ad7b7
QuanTN <tranngocquan95vn@gmail.com>
2020-07-05 18:58:06 +0700

    create layout

--- client-app/src/app/core/core.module.ts
+++ client-app/src/app/core/core.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot()
   ],
   exports: [
     NavBarComponent
