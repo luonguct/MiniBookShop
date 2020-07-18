01e0d0c31024f31da4acdc8d95333ca0a9d5365e
QuanTN <tranngocquan95vn@gmail.com>
2020-07-18 17:15:34 +0700

    Add some seed data and script to create cache table

--- src/MiniShop/Program.cs
+++ src/MiniShop/Program.cs
@@ -1,17 +1,41 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MiniShop.Infrastructure.Data;
+using System;
+using System.Threading.Tasks;
 
 namespace MiniShop.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<MiniShopDbContext>();
+                    await context.Database.MigrateAsync();
+                    await SeedDataFromJson.SeedAsync(context, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+              Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
---
+++ src/Minishop.Infrastructure/Data/SeedDataFromJson.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using MiniShop.Core.Entities;
+
+namespace MiniShop.Infrastructure.Data
+{
+    public class SeedDataFromJson
+    {
+        public static async Task SeedAsync(MiniShopDbContext context, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                if (!context.Authors.Any())
+                {
+                    var authorsData = File.ReadAllText(path + @"/Data/SeedData/authors.json");
+
+                    var authors = JsonSerializer.Deserialize<List<Author>>(authorsData);
+
+                    foreach (var item in authors)
+                    {
+                        context.Authors.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.BookCategories.Any())
+                {
+                    var bookCategoriesData = File.ReadAllText(path + @"/Data/SeedData/bookCategories.json");
+
+                    var bookCategories = JsonSerializer.Deserialize<List<BookCategory>>(bookCategoriesData);
+
+                    foreach (var item in bookCategories)
+                    {
+                        context.BookCategories.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (!context.Books.Any())
+                {
+                    var booksData = File.ReadAllText(path + @"/Data/SeedData/books.json");
+
+                    var books = JsonSerializer.Deserialize<List<Book>>(booksData);
+
+                    foreach (var item in books)
+                    {
+                        context.Books.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<SeedDataFromJson>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
